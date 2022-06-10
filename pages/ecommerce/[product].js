9446e0106aead86de2148f8f0297fe59fd03ca3b
Uddipan Bhattacharyya <uddipan.bhatta7@gmail.com>
2022-06-10 13:20:52 +0530

    Payment Gateway

--- pages/ecommerce/[product].js
+++ pages/ecommerce/[product].js
@@ -1,38 +1,69 @@
-import { useRouter } from "next/router"
+import axios from "axios";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-import keys from "../../api/config/keys"
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+
+import keys from "../../api/config/keys";
 
 export default function Product(props) {
-    // const router = useRouter()
-    // const productID = router.query.product
-    // console.log(product)
-    return (
-        <>
-            <h1>ProductID: {props.product.id}</h1>
-            <h1>{props.product.brand}</h1>
-            <h1>{props.product.title}</h1>
-        </>
-    )
+  const [open, setOpen] = useState(false);
+  // const router = useRouter()
+  // const productID = router.query.product
+  // console.log(product)
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const addToCart = async (id) => {
+    const response = await axios.get(`/api/cart/add/${id}`);
+    console.log(response);
+    if (response.status === 200) {
+      setOpen(true);
+    }
+  };
+
+  return (
+    <>
+      <Link href="/ecommerce/cart">GO TO CART</Link>
+
+      <h1>ProductID: {props.product.id}</h1>
+      <h1>{props.product.brand}</h1>
+      <h1>{props.product.title}</h1>
+      <Button onClick={() => addToCart(props.product._id)}>ADD TO CART</Button>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Added to cart"
+      />
+    </>
+  );
 }
 
 export async function getServerSideProps({ params }) {
-    const res = await fetch(keys.redirectDomain + '/api/product/' + params.product)
-    const product = await res.json()
-    return {
-        props: {product},
-    }
+  const res = await fetch(
+    keys.redirectDomain + "/api/product/" + params.product
+  );
+  const product = await res.json();
+  return {
+    props: { product },
+  };
 }
-
 
 // export async function getStaticPaths() {
 //     const res = await fetch(keys.redirectDomain + '/api/products')
 //     const products = await res.body
-  
-//     const paths = 
+
+//     const paths =
 //     products.map((product) => ({
 //       params: { product: product._id.toString() },
 //     }))
-  
+
 //     return { paths, fallback: false }
 // }
 
