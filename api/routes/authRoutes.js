3955a1db359f5ecb8e5825aa8432828459951ed2
Uddipan Bhattacharyya <uddipan.bhatta7@gmail.com>
2022-06-10 14:18:56 +0530

    check login status and redirect

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,30 +1,30 @@
 const passport = require("passport");
 
 module.exports = (router) => {
-    router.get(
-        "/auth/google",
-        passport.authenticate("google", {
-            scope: ["profile", "email"],
-        })
-    );
+  router.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
 
-    router.get(
-        "/auth/google/callback",
-        passport.authenticate("google"),
-        (req, res) => {
-            res.redirect("/");
-            console.log("auth Google");
-        }
-    );
+  router.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/ecommerce");
+      console.log("auth Google");
+    }
+  );
 
-    router.get("/api/logout", (req, res) => {
-        req.logout();
-        res.redirect("/");
-        // res.send(req.user);
-    });
+  router.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+    // res.send(req.user);
+  });
 
-    router.get("/api/current_user", (req, res) => {
-        // To check if the user is logged in.
-        res.send(req.user);
-    });
-}
+  router.get("/api/current_user", (req, res) => {
+    // To check if the user is logged in.
+    res.send(req.user);
+  });
+};
